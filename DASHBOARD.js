d3f0fc21598b2243eb0b1fb77cdf29eea911c46d
Hilton Borah <hiltonborah123@gmail.com>
2022-07-23 02:04:59 +0530

    added three pages

---
+++ DASHBOARD.js
@@ -0,0 +1,14 @@
+let useararr = JSON.parse(localStorage.getItem("Udata"))
+
+        showdata(useararr)
+
+        function showdata(data){
+            document.getElementById("hi_JS_p").innerHTML=""
+            data.forEach((el)=>{
+
+                let p=document.createElement("p")
+                p.innerText=el.email
+
+                document.getElementById("hi_JS_p").append(p)
+            })
+        }
